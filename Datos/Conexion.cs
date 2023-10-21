5a9541e19d57e7b7e23397f6c19326ddef9394a5
Romina <rominadiaz87mail.com>
2023-10-20 21:19:41 -0300

    se sube primer parte del proyecto

---
+++ Datos/Conexion.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace MudarT.Datos
+{
+    public class Conexion
+    {
+        private string cadenaSQL = string.Empty;
+        public Conexion() {         
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appSettings.json").Build();
+            cadenaSQL = builder.GetSection("ConnectionStrings:cadenaSQL").Value;
+        }
+
+        public string CadenaSQL { get {  return cadenaSQL; } }
+    }
+}
---
+++ Models/ModelRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace MudarT.Models
+{
+    public class ModelRoles
+    {
+        public int rol_id { get; set; }
+        public string? rol_nombre { get; set; }
+    }
+}
